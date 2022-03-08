17603664cd3e9b7c4526ed087ad119b856e02be4
subhasisdwary <dwary15@gmail.com>
2022-03-08 14:07:18 +0530

    Test01 is added

---
+++ SeleniumApp1/src/com/selenium/test/Test01.java
@@ -0,0 +1,11 @@
+package com.selenium.test;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
